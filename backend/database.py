6f0df73b2da9eac0aaace1e60a2facdca6135567
tankorsmash <tankorsmash@gmail.com>
2018-05-23 18:27:24 -0400

    start on database models for ratings

---
+++ backend/database.py
@@ -0,0 +1,34 @@
+import arrow
+import random
+
+from peewee import (
+    SqliteDatabase, Model, CharField, DateField, FloatField
+)
+
+from models import Rating, MODEL_DATE_FORMAT
+
+database = SqliteDatabase('ratings.db')
+
+database.connect()
+database.create_tables([Rating])
+
+def create_fake_data(count):
+    fields = [Rating.rating_score, Rating.username, Rating.created_at]
+
+    records = []
+    for i in range(count):
+        rating_score=round(random.uniform(0, 5))
+        username="test_user"
+        created_at=arrow.now().format(MODEL_DATE_FORMAT)
+
+        records.append((rating_score, username, created_at))
+
+    print("creating {} fake records".format(count))
+    with database.atomic():
+        step_range = 250 #scale down as needed
+        for step in range(0, count, step_range):
+            Rating.insert_many(records[step:step+step_range], fields=fields).execute()
+    print("done creating fake records")
+
+create_fake_data(10000)
+
---
+++ backend/models.py
@@ -0,0 +1,14 @@
+from peewee import (
+    SqliteDatabase, Model, CharField, DateTimeField, FloatField
+)
+
+MODEL_DATE_FORMAT = "YYYY-MM-DD HH:MM:SS" #default peewee format
+
+database = SqliteDatabase('ratings.db')
+class Rating(Model):
+    rating_score = FloatField()
+    username = CharField()
+    created_at = DateTimeField(formats=[MODEL_DATE_FORMAT])
+
+    class Meta:
+        database = database
