aa89f59712fa3d99f0cbbf7cac13611fbb42d36e
tankorsmash <tankorsmash@gmail.com>
2018-05-23 15:08:19 -0400

    remove default react config from app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader'
 
-import logo from './logo.svg';
-import './App.css';
-
 class App extends Component {
 
   state = {
@@ -18,14 +15,8 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h3> Welcome to ZOMBOCOM </h3>
         <p>
           API Response: {this.state.hello}
         </p>
