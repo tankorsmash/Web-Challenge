bf2e75ea291a599259193b22959fbeb8179a56df
tankorsmash <tankorsmash@gmail.com>
2018-05-23 16:07:20 -0400

    whitespace

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,26 +3,26 @@ import { hot } from 'react-hot-loader'
 
 class App extends Component {
 
-  state = {
-    hello: ""
-  }
+    state = {
+        hello: ""
+    }
 
-  async componentDidMount() {
-    const res = await fetch("/hello")
-    const json = await res.json()
-    this.setState(json)
-  }
+    async componentDidMount() {
+        const res = await fetch("/hello")
+        const json = await res.json()
+        this.setState(json)
+    }
 
-  render() {
-    return (
-      <div className="container">
-        <h3> Welcome to ZOMBOCOM </h3>
-        <p>
-          API Response: {this.state.hello}
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container">
+                <h3> Welcome to ZOMBOCOM </h3>
+                <p>
+                    API Response: {this.state.hello}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default hot(module)(App)
