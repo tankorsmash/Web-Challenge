54337add8f433cfc11c9e1aec834bf9daad78d51
tankorsmash <tankorsmash@gmail.com>
2018-05-23 14:55:00 -0400

    hook in hot reloading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader'
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -32,4 +34,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default hot(module)(App)
