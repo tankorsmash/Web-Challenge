d4cad5dc4d0117ab998aa4476f9ecb29d2217e9e
tankorsmash <tankorsmash@gmail.com>
2018-05-25 13:03:58 -0400

    remove unused include

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader'
 import {
-    NavbarBrand, Navbar, NavbarToggler, Collapse
+    NavbarBrand, Navbar, Collapse
 } from 'reactstrap';
 import './App.css';
 
