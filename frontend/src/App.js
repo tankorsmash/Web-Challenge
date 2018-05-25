501afdabc04d435282844d9ea2c39f4d58f84b69
tankorsmash <tankorsmash@gmail.com>
2018-05-25 11:10:25 -0400

    move state into RatingChart, add refresh button
    the button only gets the existing data, TODO make it use refresh_ratings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader'
 import {
-    Collapse, Media, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem,
-    NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem
+    NavbarBrand, Navbar
 } from 'reactstrap';
 import './App.css';
 
@@ -29,13 +28,8 @@ class GamedevTvNavbar extends Component {
             <div>
                 <Navbar id="gamedevtv-navbar" color="light" light expand="md">
                     <NavbarBrand href="/">
-                        <img src="https://discourse-cdn-sjc1.com/business5/uploads/gamedev/optimized/1X/2d1f1721ff9a04b33e75ce2e606e124626104249_1_690x149.png"/>
+                        <img alt="GamedevTV logo" src="https://discourse-cdn-sjc1.com/business5/uploads/gamedev/optimized/1X/2d1f1721ff9a04b33e75ce2e606e124626104249_1_690x149.png"/>
                     </NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                        </Nav>
-                    </Collapse>
                 </Navbar>
             </div>
         );
@@ -43,37 +37,13 @@ class GamedevTvNavbar extends Component {
 };
 
 class App extends Component {
-    state = {
-        chartData: {
-            date_labels: [],
-            average_axis: [],
-            count_axis: [],
-        },
-    }
-
-    async updateData() {
-        const res = await fetch("/ratings")
-        const json = await res.json()
-        this.setState({
-            chartData: json.chart_data
-        })
-    }
-
-    async componentDidMount() {
-        this.updateData();
-    }
-
     render() {
-        let rawChartData = this.state.chartData;
-
         return (
             <div className="">
                 <GamedevTvNavbar/>
                 <div className="container">
                     <h3>Ratings over time</h3>
-                    <RatingChart dateLabels={rawChartData.date_labels} averageAxis={rawChartData.average_axis} countAxis={rawChartData.count_axis}  />
-                    <div>
-                    </div>
+                    <RatingChart/>
                 </div>
             </div>
         );
--- frontend/src/components/RatingChart.js
+++ frontend/src/components/RatingChart.js
@@ -1,30 +1,51 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+
+import { Button } from 'reactstrap';
 import { Line } from 'react-chartjs-2';
 
 export default class RatingChart extends React.Component {
-    static propTypes = {
-        dateLabels: PropTypes.array.isRequired,
-        averageAxis: PropTypes.array.isRequired,
-        countAxis: PropTypes.array.isRequired,
+    state = {
+        chartData: {
+            date_labels: [],
+            average_axis: [],
+            count_axis: [],
+        },
     }
 
+    updateData = async () => {
+        this.setState({
+            buttonText: "Refreshing...",
+        });
+        const res = await fetch("/fetch_ratings")
+        const json = await res.json()
+        this.setState({
+            chartData: json.chart_data,
+            buttonText: "Refresh",
+        })
+    }
+
+    async componentDidMount() {
+        this.updateData();
+    }
+
+
     render() {
-        let {dateLabels, averageAxis, countAxis} = {...this.props};
+        let {date_labels, average_axis, count_axis} = {...this.state.chartData};
 
         const limit = 1000;
         let chartData = {
-            labels: dateLabels.slice(0, limit),
+            labels: date_labels.slice(0, limit),
             datasets: [{
                 label: "Averages",
-                data: averageAxis.slice(0, limit),
+                data: average_axis.slice(0, limit),
                 borderColor: "rgb(255, 99, 132)",
                 backgroundColor: "rgb(255, 99, 132)",
                 fill: false,
                 yAxisID: "y-axis-avg",
             },{
                 label: "Total Ratings",
-                data: countAxis.slice(0, limit),
+                data: count_axis.slice(0, limit),
                 fill: false,
                 borderColor: "rgb(54, 162, 235)",
                 backgroundColor: "rgb(54, 162, 235)",
@@ -57,7 +78,8 @@ export default class RatingChart extends React.Component {
 
         return (
             <div>
-                <Line  options={chartOptions} data={chartData} />
+                <Button onClick={this.updateData}> {this.state.buttonText} </Button>
+                <Line options={chartOptions} data={chartData} />
             </div>
         );
     }
