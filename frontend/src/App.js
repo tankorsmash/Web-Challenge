afadf9bcced1d0f1756d4a3dec1c51e5f1777c0b
tankorsmash <tankorsmash@gmail.com>
2018-05-24 21:25:25 -0400

    extra RatingChart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader'
-import { Line } from 'react-chartjs-2';
+
+import RatingChart from './components/RatingChart';
 
 class App extends Component {
     state = {
@@ -12,7 +13,7 @@ class App extends Component {
         },
     }
 
-    async componentDidMount() {
+    async updateData() {
         const res = await fetch("/ratings")
         const json = await res.json()
         this.setState({
@@ -20,56 +21,17 @@ class App extends Component {
         })
     }
 
+    async componentDidMount() {
+        this.updateData();
+    }
+
     render() {
         let rawChartData = this.state.chartData;
-
-        const limit = 1000;
-        let chartData = {
-            labels: rawChartData.date_labels.slice(0, limit),
-            datasets: [{
-                label: "Averages",
-                data: rawChartData.average_axis.slice(0, limit),
-                borderColor: "rgb(255, 99, 132)",
-                backgroundColor: "rgb(255, 99, 132)",
-                fill: false,
-                yAxisID: "y-axis-avg",
-            },{
-                label: "Total Ratings",
-                data: rawChartData.count_axis.slice(0, limit),
-                fill: false,
-                borderColor: "rgb(54, 162, 235)",
-                backgroundColor: "rgb(54, 162, 235)",
-                yAxisID: "y-axis-count",
-            }]
-        };
-
-        let chartOptions = {
-            responsive: true,
-            scales: {
-                stacked: false,
-                yAxes: [{
-                    type: "linear",
-                    position: "left",
-                    display: true,
-                    id: "y-axis-avg",
-                }, {
-                    type: "linear",
-                    display: true,
-                    position: "right",
-                    id: "y-axis-count",
-
-                    // grid line settings
-                    gridLines: {
-                        drawOnChartArea: false, // only want the grid lines for one axis to show up
-                    },
-                }],
-            },
-        };
 
         return (
             <div className="container">
                 <h3> Hourly average </h3>
-                <Line  options={chartOptions} data={chartData} />
+                <RatingChart dateLabels={rawChartData.date_labels} averageAxis={rawChartData.average_axis} countAxis={rawChartData.count_axis}  />
                 <div>
                 </div>
             </div>
---
+++ frontend/src/components/RatingChart.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Line } from 'react-chartjs-2';
+
+export default class RatingChart extends React.Component {
+    static propTypes = {
+        dateLabels: PropTypes.array.isRequired,
+        averageAxis: PropTypes.array.isRequired,
+        countAxis: PropTypes.array.isRequired,
+    }
+
+    render() {
+        let {dateLabels, averageAxis, countAxis} = {...this.props};
+
+        const limit = 1000;
+        let chartData = {
+            labels: dateLabels.slice(0, limit),
+            datasets: [{
+                label: "Averages",
+                data: averageAxis.slice(0, limit),
+                borderColor: "rgb(255, 99, 132)",
+                backgroundColor: "rgb(255, 99, 132)",
+                fill: false,
+                yAxisID: "y-axis-avg",
+            },{
+                label: "Total Ratings",
+                data: countAxis.slice(0, limit),
+                fill: false,
+                borderColor: "rgb(54, 162, 235)",
+                backgroundColor: "rgb(54, 162, 235)",
+                yAxisID: "y-axis-count",
+            }]
+        };
+
+        let chartOptions = {
+            responsive: true,
+            scales: {
+                stacked: false,
+                yAxes: [{
+                    type: "linear",
+                    position: "left",
+                    display: true,
+                    id: "y-axis-avg",
+                }, {
+                    type: "linear",
+                    display: true,
+                    position: "right",
+                    id: "y-axis-count",
+
+                    // grid line settings
+                    gridLines: {
+                        drawOnChartArea: false, // only want the grid lines for one axis to show up
+                    },
+                }],
+            },
+        };
+
+        return (
+            <div className="container">
+                <h3> Hourly average </h3>
+                <Line  options={chartOptions} data={chartData} />
+                <div>
+                </div>
+            </div>
+        );
+    }
+}
