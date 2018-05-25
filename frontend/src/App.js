a21ed5619ddd663facb9e214a93e7c5dbe0a89b6
tankorsmash <tankorsmash@gmail.com>
2018-05-25 11:35:44 -0400

    clean up header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,27 +2,13 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader'
 import {
-    NavbarBrand, Navbar
+    NavbarBrand, Navbar, NavbarToggler, Collapse
 } from 'reactstrap';
 import './App.css';
 
 import RatingChart from './components/RatingChart';
 
 class GamedevTvNavbar extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isOpen: false
-        };
-    }
-
-    toggle = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
-
     render() {
         return (
             <div>
@@ -30,6 +16,7 @@ class GamedevTvNavbar extends Component {
                     <NavbarBrand href="/">
                         <img alt="GamedevTV logo" src="https://discourse-cdn-sjc1.com/business5/uploads/gamedev/optimized/1X/2d1f1721ff9a04b33e75ce2e606e124626104249_1_690x149.png"/>
                     </NavbarBrand>
+                    <Collapse navbar/>
                 </Navbar>
             </div>
         );
